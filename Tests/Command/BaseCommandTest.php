b126dc1c790d87d3c926f15ce703365a9947e095
Theodor Manolescu <thedor.manolescu@emag.ro>
2020-02-28 11:43:53 +0200

    #15 Fix deprecations

--- Tests/Command/BaseCommandTest.php
+++ Tests/Command/BaseCommandTest.php
@@ -2,25 +2,34 @@
 
 namespace OldSound\RabbitMqBundle\Tests\Command;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\InputDefinition;
 
 abstract class BaseCommandTest extends TestCase
 {
+    /** @var MockObject|Application */
     protected $application;
+    /** @var MockObject|InputDefinition */
     protected $definition;
+    /** @var MockObject|HelperSet */
     protected $helperSet;
     protected $command;
 
+    /**
+     * @inheritDoc
+     */
     protected function setUp(): void
     {
-        $this->application = $this->getMockBuilder('Symfony\\Component\\Console\\Application')
+        $this->application = $this->getMockBuilder(Application::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $this->definition = $this->getMockBuilder('Symfony\\Component\\Console\\Input\\InputDefinition')
+        $this->definition = $this->getMockBuilder(InputDefinition::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $this->helperSet = $this->getMockBuilder('Symfony\\Component\\Console\\Helper\\HelperSet')->getMock();
-
+        $this->helperSet = $this->getMockBuilder(HelperSet::class)->getMock();
         $this->application->expects($this->any())
             ->method('getDefinition')
             ->will($this->returnValue($this->definition));
